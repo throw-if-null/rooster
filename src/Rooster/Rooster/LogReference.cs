7b03b456bfd3533a4b966571c85643fe4b9fd47c
Mirza Merdovic <mirzamerdovic@gmail.com>
2020-05-13 00:08:09 +0200

    Initial stuff

---
+++ src/Rooster/Rooster/LogReference.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Rooster
+{
+    public class LogReference
+    {
+        [JsonProperty("machineName")]
+        public string MachineName { get; set; }
+
+        [JsonProperty("lastUpdated")]
+        public DateTimeOffset LastUpdated { get; set; }
+
+        [JsonProperty("size")]
+        public int Size { get; set; }
+
+        [JsonProperty("href")]
+        public string Href { get; set; }
+
+        [JsonProperty("path")]
+        public string Path { get; set; }
+    }
+}
---
+++ src/Rooster/Rooster/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rooster
+{
+    class Program
+    {
+        private const string LogsPath = @"https://bf-studioapi-sandbox.scm.azurewebsites.net/api/logs/docker";
+
+        internal static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
